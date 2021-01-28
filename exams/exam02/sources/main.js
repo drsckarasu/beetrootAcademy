7813328df4b4e24cdf6b9e84760783e5b5137040
drsckarasu <drsckarasu@gmail.com>
2021-01-28 13:48:49 +0200

    finished with uograted form

--- exams/exam02/sources/main.js
+++ exams/exam02/sources/main.js
@@ -233,39 +233,9 @@ function initMap() {
   });
 }
 
-$(function() {
-  $('.error').hide();
-  $(".button").click(function() {
-
-    $('.error').hide();
-  	  var name = $("input#name").val();
-  		if (name == "") {
-        $("label#name_error").show();
-        $("input#name").focus();
-        return false;
-      }
-  		var email = $("input#email").val();
-  		if (email == "") {
-        $("label#email_error").show();
-        $("input#email").focus();
-        return false;
-      }
-      /*var dataString = 'name='+ name + '&email=' + email;
-      $.ajax({
-        type: "POST",
-        url: "bin/process.php",
-        data: dataString,
-        success: function() {
-          $('#contact_form').html("<div id='message'></div>");
-          $('#message').html("<h2>Contact Form Submitted!</h2>")
-          .append("<p>We will be in touch soon.</p>")
-          .hide()
-          .fadeIn(1500, function() {
-            $('#message').append("<img id='checkmark' src='images/check.png' />");
-          });
-        }
-      });
-      return false;*/
+$(document).ready(function() {
+  $("#basic-form").validate({
+    validClass: "valid success-alert",
   });
 });
 
@@ -275,6 +245,6 @@ function navigation(){
     const burger = document.querySelector('.navigation__content_burger');
     const navigation = document.querySelector('.navigation__content');
     burger.addEventListener('click', ()=>{
-        navigation.classList.toggle('shown')
+        navigation.classList.toggle('shown');
     })
 }
